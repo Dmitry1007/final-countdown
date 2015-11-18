3a60a2684ae09b1defa423321ea19da51ae5c035
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 11:23:31 -0700

    user model tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  validates :email_address, uniqueness: true, presence: true
+  validates :password, presence: true, length: { in: 3..15 }, confirmation: true
+
   has_secure_password
   has_many :links
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context "a registered user" do
+    let!(:user) { User.create(email_address: "user@gmail.com", password: "password" ) }
+
+    it "is valid" do
+      expect(user).to be_valid
+    end
+  end
+
+  context "an invalid user" do
+    let!(:user) { User.create(email_address: "user@gmail.com", password: "password" ) }
+
+    it "is invalid without an email_address" do
+      user.email_address = nil
+      expect(user).to_not be_valid
+    end
+
+    it "is invalid without a password" do
+      user.password = nil
+      expect(user).to_not be_valid
+    end
+  end
 end
