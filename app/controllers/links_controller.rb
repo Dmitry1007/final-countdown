a6b7babbc64f92b73473617d796e4e6973e9f61c
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 12:31:37 -0700

    display current_users link on page

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -2,4 +2,16 @@ class LinksController < ApplicationController
   def index
     @links = current_user.links
   end
+
+  def create
+    @link = Link.create!(title: params[:link][:title], url: params[:link][:url], user_id: current_user.id)
+
+    if @link
+      flash[:success] = "You've created a new link"
+      redirect_to :back
+    else
+      flash[:danger] = "There was problem creating your link, try again"
+      redirect_to :back
+    end
+  end
 end
