893d1e3146957d46fd8a06b078ba9e07928593c0
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 11:48:13 -0700

    link model tests

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,4 +1,5 @@
 class Link < ActiveRecord::Base
+  validates :title, presence: true
   validates :url, :format => URI::regexp(%w(http https))
 
   belongs_to :user
--- spec/models/link_spec.rb
+++ spec/models/link_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Link, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let!(:user) { User.create!(email_address: "user@gmail.com", password: "password" ) }
+  let!(:link) { Link.create!(title: "badass link", url: "http://do-watch-want.com", user_id: user.id) }
+
+  it "has a user" do
+    expect(Link.first.user_id).to eq(user.id)
+  end
+
+  it "has a title" do
+    expect(link.title).to eq("badass link")
+  end
+
+  it "defaults to false" do
+    expect(link.read).to eq(false)
+  end
+
+  it "is valid with valid url" do
+    expect(link).to be_valid
+  end
+
+  it "is not valid with invalid url" do
+    link.url = "ht://do-watcha-want.com"
+    expect(link).to_not be_valid
+  end
 end
