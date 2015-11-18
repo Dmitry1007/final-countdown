6a3253ad0955459b8301871e75c469d000b4b4f3
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 13:19:56 -0700

    iteration 4, editing links complete

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -4,14 +4,30 @@ class LinksController < ApplicationController
   end
 
   def create
-    @link = Link.create!(title: params[:link][:title], url: params[:link][:url], user_id: current_user.id)
+    @link = Link.create(title: params[:link][:title], url: params[:link][:url], user_id: current_user.id)
 
-    if @link
+    if @link.save
       flash[:success] = "You've created a new link"
       redirect_to :back
     else
       flash[:danger] = "There was problem creating your link, try again"
       redirect_to :back
+    end
+  end
+
+  def edit
+    @link = Link.find(params[:id])
+  end
+
+  def update
+    @link = Link.find(params[:id])
+    @link.update(title: params[:link][:title], url: params[:link][:url], user_id: current_user.id)
+
+    if @link.save
+      redirect_to links_path
+    else
+      flash[:danger] = "Please enter a valid URL"
+      render :edit
     end
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,6 +1,6 @@
 class Link < ActiveRecord::Base
   validates :title, presence: true
-  validates :url, :format => URI::regexp(%w(http https))
+  validates :url, :format => URI::regexp(%w(http https)), presence: true
 
   belongs_to :user
 end
