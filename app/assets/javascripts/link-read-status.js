569534505457fc0af0a4a5412bba2e2afcc12592
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 14:35:46 -0700

    iteration 3 with Ajax request to internal API endpoint complete

--- app/assets/javascripts/link-read-status.js
+++ app/assets/javascripts/link-read-status.js
@@ -1,13 +1,16 @@
 $(document).ready(function () {
   $(".read-status").on("click", function () {
     var linkId = $.parseJSON($(this).attr('data'));
-    console.log(linkId)
 
     $.ajax({
       type:    "POST",
       url:     "/api/v1/links/" + linkId,
       success: function(link) {
-        console.log(link)
+        if (link.read === true) {
+          $("button[data="+link.id+"]").text("Mark As Unread")
+        } else {
+          $("button[data="+link.id+"]").text("Mark As Read")
+        }
       }
     })
   })
