54ec10ecb6bf2e833b535e33cd815c66e59c8c59
Dmitry Sky <dmitry.sky@icloud.com>
2015-11-18 21:07:59 -0700

    comment out ajax request for all user links

--- app/assets/javascripts/fetch-user-links.js
+++ app/assets/javascripts/fetch-user-links.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-  $.ajax({
-    type:    "GET",
-    url:     "/api/v1/index",
-    success: function(links) {
-      links.forEach(function (link) {
-        if (link.read === false){
-          var readStatus = "Mark As Read"
-        } else {
-          var readStatus = "Mark As Unread"
-        }
+// $(document).ready(function () {
+//   $.ajax({
+//     type:    "GET",
+//     url:     "/api/v1/index",
+//     success: function(links) {
+//       links.forEach(function (link) {
+//         if (link.read === false){
+//           var readStatus = "Mark As Read"
+//         } else {
+//           var readStatus = "Mark As Unread"
+//         }
 
-        $("#links-container").append(
-          "<div class='link' data-title="+link.title+" data-id='"
-          + link.id
-          + "'><h6>Title: "
-          + link.title
-          + "</h6><span data="+link.id+">"
-          + link.url
-          + "</span><br><a href=/links/"+link.id+"/edit>Edit</a>"
-          + "<button id=read-status data="+link.id+">"+readStatus+"</button></div>"
-        )
-      })
-    }
-  })
-})
+//         $("#links-container").append(
+//           "<div class='link' data-title="+link.title+" data-id='"
+//           + link.id
+//           + "'><h6>Title: "
+//           + link.title
+//           + "</h6><span data="+link.id+">"
+//           + link.url
+//           + "</span><br><a href=/links/"+link.id+"/edit>Edit</a>"
+//           + "<button id=read-status data="+link.id+">"+readStatus+"</button></div>"
+//         )
+//       })
+//     }
+//   })
+// })
